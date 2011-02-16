8e152dfc759ce8995dbd926ca9ac560fb5619bfe
Richard Crowley <r@rcrowley.org>
2011-02-16 17:56:37 +0000

    Successful ctime-based false positive removal.
    If a file shares the same nanosecond-resolution ctime with other files in the directory, it's a safe bet they were placed by package managers or the OS install and are therefore OK to ignore.  The blueprintignore(5) rules are still in effect.

--- blueprint/backend/files.py
+++ blueprint/backend/files.py
@@ -3,6 +3,7 @@ Search for configuration files to include in the blueprint.
 """
 
 import base64
+from collections import defaultdict
 import fnmatch
 import glob
 import grp
@@ -13,25 +14,39 @@ import pwd
 import stat
 import subprocess
 
-EXCLUDE = ('/etc/alternatives',
-           '/etc/group-',
-           '/etc/group',
-           '/etc/gshadow-',
-           '/etc/gshadow',
-           '/etc/ld.so.cache',
-           '/etc/mtab',
-           '/etc/passwd-',
-           '/etc/passwd',
-           '/etc/rc0.d',
-           '/etc/rc1.d',
-           '/etc/rc2.d',
-           '/etc/rc3.d',
-           '/etc/rc4.d',
-           '/etc/rc5.d',
-           '/etc/rc6.d',
-           '/etc/rcS.d',
-           '/etc/shadow-',
-           '/etc/shadow')
+# The default list of ignore patterns.  Update `blueprintignore`(5) if you
+# make changes here.
+IGNORE = ('/etc/alternatives',
+          '/etc/apparmor.d/cache',
+          '/etc/ca-certificates.conf',
+          '/etc/group-',
+          '/etc/group',
+          '/etc/gshadow-',
+          '/etc/gshadow',
+          '/etc/initramfs-tools/conf.d/resume',
+          '/etc/ld.so.cache',
+          '/etc/mtab',
+          '/etc/passwd-',
+          '/etc/passwd',
+          '/etc/rc0.d',
+          '/etc/rc1.d',
+          '/etc/rc2.d',
+          '/etc/rc3.d',
+          '/etc/rc4.d',
+          '/etc/rc5.d',
+          '/etc/rc6.d',
+          '/etc/rcS.d',
+          '/etc/shadow-',
+          '/etc/shadow')
+
+# An extra list of pathnames and MD5 sums that will be checked after no
+# match is found in `dpkg`(1)'s list.  If a pathname is given as the value
+# then that file's contents will be hashed.
+MD5SUMS = {'/etc/apparmor.d/tunables/home.d/ubuntu':
+               '2a88811f7b763daa96c20b20269294a4',
+           '/etc/iscsi/iscsid.conf': '6c6fd718faae84a4ab1b276e78fea471',
+           '/etc/ppp/peers/provider': '/usr/share/ppp/provider.peer',
+           '/etc/python/debian_config': '7f4739eb8858d231601a5ed144099ac8'}
 
 def files(b):
     logging.info('searching for configuration files')
@@ -39,26 +54,34 @@ def files(b):
     # Visit every file in `/etc` except those on the exclusion list above.
     for dirpath, dirnames, filenames in os.walk('/etc'):
 
-        # Don't even bother recursing into hard-coded excluded directories.
-        if dirpath in EXCLUDE:
-            del dirnames[:]
-            continue
-
         # Determine if this entire directory should be ignored by default.
-        # FIXME This doesn't actually work right without a recursive
-        # implementation.  (Ignoring /foo won't currently affect /foo/bar.)
         ignored = _ignore(os.path.basename(dirpath), dirpath)
 
-        for filename in filenames:
-            pathname = os.path.join(dirpath, filename)
+        # Track the ctime of each file in this directory.  Weed out false
+        # positives by ignoring files with common ctimes.
+        ctimes = defaultdict(lambda: 0)
 
-            # Ignore hard-coded excluded files.
-            if pathname in EXCLUDE:
-                continue
+        # Collect up the full pathname to each file and `lstat` them all.
+        files = [(pathname, os.lstat(pathname))
+                 for pathname in [os.path.join(dirpath, filename)
+                                  for filename in filenames]]
+
+        # Map the ctimes of each directory entry.
+        for pathname, s in files:
+            ctimes[s.st_ctime] += 1
+        for dirname in dirnames:
+            ctimes[os.lstat(os.path.join(dirpath, dirname)).st_ctime] += 1
+
+        for pathname, s in files:
 
             # Ignore files that match in the `gitignore`(5)-style
-            # `~/.blueprintignore` file.
-            if _ignore(filename, pathname, ignored=ignored):
+            # `~/.blueprintignore` file.  Default to ignoring files that
+            # share their ctime with other files in the directory.  This
+            # is a very strong indication that the file is original to
+            # the system and should be ignored.
+            if _ignore(filename,
+                       pathname,
+                       ignored=ignored or 1 < ctimes[s.st_ctime]):
                 continue
 
             # The content is used even for symbolic links to determine whether
@@ -69,9 +92,17 @@ def files(b):
                 #logging.warning('{0} not readable'.format(pathname))
                 continue
 
-            # Don't store files which are part of a package and are unchanged
-            # from the distribution.
-            if hashlib.md5(content).hexdigest() == _md5(pathname):
+            # Ignore files that are unchanged from their packaged version
+            # or match in the `MD5SUMS` dict.
+            md5sum = _dpkg_md5sum(pathname)
+            if md5sum is None and pathname in MD5SUMS:
+                md5sum = MD5SUMS[pathname]
+                if '/' == md5sum[0]:
+                    try:
+                        md5sum = hashlib.md5(open(md5sum).read()).hexdigest()
+                    except IOError:
+                        md5sum = None
+            if hashlib.md5(content).hexdigest() == md5sum:
                 if _ignore(filename, pathname, ignored=True):
                     continue
 
@@ -85,7 +116,8 @@ def files(b):
                 except IOError:
                     pass
 
-            s = os.lstat(pathname)
+            # At this point, it's almost certain the file is going to be
+            # included in the blueprint.
 
             # A symbolic link's content is the link target.
             if stat.S_ISLNK(s.st_mode):
@@ -130,7 +162,7 @@ def _ignore(filename, pathname, ignored=False):
 
     # Cache the patterns stored in the `~/.blueprintignore` file.
     if not hasattr(_ignore, '_cache'):
-        _ignore._cache = []
+        _ignore._cache = [(pattern, False) for pattern in IGNORE]
         try:
             for pattern in open(os.path.expanduser('~/.blueprintignore')):
                 pattern = pattern.rstrip()
@@ -172,7 +204,7 @@ def _ignore(filename, pathname, ignored=False):
 
     return ignored
 
-def _md5(pathname):
+def _dpkg_md5sum(pathname):
     """
     Find the MD5 sum of the packaged version of pathname or `None` if the
     `pathname` does not come from a Debian package.
