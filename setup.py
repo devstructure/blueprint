934370017527c7339c9437e8b3b806d66b495236
Richard Crowley <r@rcrowley.org>
2011-02-16 22:59:22 +0000

    Python build tools.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import re
+
+pattern = re.compile(r'^VERSION=(.+)$')
+version = None
+for line in open('Makefile'):
+    match = pattern.match(line)
+    if match is None:
+        continue
+    version = match.group(1)
+    break
+if version is None:
+    raise EnvironmentError, '/^VERSION=/ not matched in Makefile.'
+
+setup(name='blueprint',
+      version=version,
+      description='reverse engineer server configuration',
+      author='Richard Crowley',
+      author_email='richard@devstructure.com',
+      url='http://devstructure.com/',
+      packages=find_packages(),
+      license='BSD',
+      zip_safe=True)
