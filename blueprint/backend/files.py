9bee0d5601d6fb6f9b1201c70469b5d613b1b615
Richard Crowley <r@rcrowley.org>
2011-03-23 15:12:17 +0000

    Using *.dpkg-dist, not *.*.dpkg-dist.
    mrud didn't have a special reason for this and I see no reason to limit ignored files to files that have an extension in addition to .dpkg-dist.

--- blueprint/backend/files.py
+++ blueprint/backend/files.py
@@ -17,7 +17,7 @@ import subprocess
 # The default list of ignore patterns.
 #
 # XXX Update `blueprintignore`(5) if you make changes here.
-IGNORE = ('*.*.dpkg-dist',
+IGNORE = ('*.dpkg-dist',
           '*.dpkg-old',
           '/etc/.git',
           '/etc/.pwd.lock',
