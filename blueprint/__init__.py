398d3c17bb45ebc0bc0a92a5af2bdf441b018929
Richard Crowley <r@rcrowley.org>
2011-04-12 16:31:11 +0000

    RPM-based distros don't have lsb_release.
    Deal gracefully with this fact.

--- blueprint/__init__.py
+++ blueprint/__init__.py
@@ -608,13 +608,19 @@ def lsb_release_codename():
     """
     if hasattr(lsb_release_codename, '_cache'):
         return lsb_release_codename._cache
-    p = subprocess.Popen(['lsb_release', '-c'], stdout=subprocess.PIPE)
+    try:
+        p = subprocess.Popen(['lsb_release', '-c'], stdout=subprocess.PIPE)
+    except OSError:
+        lsb_release_codename._cache = None
+        return lsb_release_codename._cache
     stdout, stderr = p.communicate()
     if 0 != p.returncode:
-        return None
+        lsb_release_codename._cache = None
+        return lsb_release_codename._cache
     match = re.search(r'\t(\w+)$', stdout)
     if match is None:
-        return None
+        lsb_release_codename._cache = None
+        return lsb_release_codename._cache
     lsb_release_codename._cache = match.group(1)
     return lsb_release_codename._cache
 
@@ -624,7 +630,8 @@ def rubygems_update():
     Determine whether the `rubygems-update` gem is needed.  It is needed
     on Lucid and older systems.
     """
-    return lsb_release_codename()[0] < 'm'
+    codename = lsb_release_codename()
+    return codename is not None and codename[0] >= 'm'
 
 
 def rubygems_virtual():
@@ -632,13 +639,14 @@ def rubygems_virtual():
     Determine whether RubyGems is baked into the Ruby 1.9 distribution.
     It is on Maverick and newer systems.
     """
-    return lsb_release_codename()[0] >= 'm'
+    codename = lsb_release_codename()
+    return codename is not None and codename[0] >= 'm'
 
 
 def rubygems_path():
     """
     Determine based on the OS release where RubyGems will install gems.
     """
-    if rubygems_update:
+    if lsb_release_codename() is None or rubygems_update():
         return '/usr/lib/ruby/gems'
     return '/var/lib/gems'
