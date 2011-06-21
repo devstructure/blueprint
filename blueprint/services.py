83b88e7277912320d519139b01fb6f649a259478
Richard Crowley <r@rcrowley.org>
2011-06-21 15:29:18 +0000

    Get back a level of indentation.

--- blueprint/services.py
+++ blueprint/services.py
@@ -10,30 +10,31 @@ import subprocess
 
 
 def _service(b, manager, service, deps):
-    if 'packages' in deps:
+    if 'packages' not in deps:
+        return
 
-        # Build a map of the directory that contains each file in the
-        # blueprint to the pathname of that file.
-        dirs = dict([(os.path.dirname(pathname), pathname)
-                     for pathname in b.files])
-        for dirname in ('/etc', '/etc/init', '/etc/init.d'):
-            dirs.pop(dirname, None)
+    # Build a map of the directory that contains each file in the
+    # blueprint to the pathname of that file.
+    dirs = dict([(os.path.dirname(pathname), pathname)
+                 for pathname in b.files])
+    for dirname in ('/etc', '/etc/init', '/etc/init.d'):
+        dirs.pop(dirname, None)
 
-        # Add dependencies for every file in the blueprint that's also in
-        # this service's package or in a directory in this service's package.
-        for package in deps['packages']:
-            try:
-                p = subprocess.Popen(['dpkg-query', '-L', package],
-                                     close_fds=True, stdout=subprocess.PIPE)
-            except OSError as e:
-                p = subprocess.Popen(['rpm', '-ql', package],
-                                     close_fds=True, stdout=subprocess.PIPE)
-            for line in p.stdout:
-                pathname = line.rstrip()
-                if pathname in b.files:
-                    b.services[manager][service]['files'].add(pathname)
-                elif pathname in dirs:
-                    b.services[manager][service]['files'].add(dirs[pathname])
+    # Add dependencies for every file in the blueprint that's also in
+    # this service's package or in a directory in this service's package.
+    for package in deps['packages']:
+        try:
+            p = subprocess.Popen(['dpkg-query', '-L', package],
+                                 close_fds=True, stdout=subprocess.PIPE)
+        except OSError as e:
+            p = subprocess.Popen(['rpm', '-ql', package],
+                                 close_fds=True, stdout=subprocess.PIPE)
+        for line in p.stdout:
+            pathname = line.rstrip()
+            if pathname in b.files:
+                b.services[manager][service]['files'].add(pathname)
+            elif pathname in dirs:
+                b.services[manager][service]['files'].add(dirs[pathname])
 
 
 def services(b):
